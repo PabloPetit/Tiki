053b9cdcd15dca08462d97c5113298b9f6300a24
PabloPetit <pablopetit94@gmail.com>
2016-07-20 12:17:54 +0200

    running

--- Android/Tiki/app/src/main/java/com/example/pablo/tiki/Connect.java
+++ Android/Tiki/app/src/main/java/com/example/pablo/tiki/Connect.java
@@ -13,27 +13,29 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Connect extends AsyncTask {
 
-    public static String LOGD_REF = "CONNECT";
+    public static String LOG_TAG = "CONNECT";
+    public static AtomicBoolean runnig = new AtomicBoolean(false);
 
 
     public boolean checkInternetConnexion(MainMenu main){
-        Log.d(LOGD_REF,"Checking internet connexion");
+        Log.d(LOG_TAG,"Checking internet connexion");
         ConnectivityManager connMgr = (ConnectivityManager)main.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            Log.d(LOGD_REF,"Connected to internet");
+            Log.d(LOG_TAG,"Connected to internet");
             return true;
         } else {
-            Log.d(LOGD_REF,"No internet connexion");
+            Log.d(LOG_TAG,"No internet connexion");
             return false;
         }
     }
 
     public boolean connexionToServer(SharedPreferences settings){
-        Log.d(LOGD_REF,"Connecting to server...");
+        Log.d(LOG_TAG,"Connecting to server...");
         String ip = settings.getString(Settings.IP,Settings.DEFAULT_IP);
         int port = settings.getInt(Settings.PORT,Settings.DEFAULT_PORT);
         try{
@@ -42,7 +44,7 @@ public class Connect extends AsyncTask {
             Connexion.output = new ObjectOutputStream(Connexion.socket.getOutputStream());
             Connexion.connected.set(true);
         }catch (IOException e){
-            Log.d(LOGD_REF,"Could not open socket");
+            Log.d(LOG_TAG,"Could not open socket");
             e.printStackTrace();
             return false;
         }
@@ -50,7 +52,7 @@ public class Connect extends AsyncTask {
     }
 
     public boolean simpleLoggin(SharedPreferences settings){
-        Log.d(LOGD_REF,"Trying to log on server...");
+        Log.d(LOG_TAG,"Trying to log on server...");
 
         String password = settings.getString(Settings.PASSWORD,Settings.DEFAULT_PASSWORD);
         String name = settings.getString(Settings.NAME,Settings.DEFAULT_NAME);
@@ -59,12 +61,12 @@ public class Connect extends AsyncTask {
 
         Proto server_name = readProto();
         if (server_name == null){
-            Log.d(LOGD_REF,"Server name not received");
+            Log.d(LOG_TAG,"Server name not received");
             return false;
         }
         Connexion.serverName = (String) server_name.getData().get("NAME");
-        Log.d(LOGD_REF,"Server name received : "+Connexion.serverName);
-        Log.d(LOGD_REF,"Sending log data");
+        Log.d(LOG_TAG,"Server name received : "+Connexion.serverName);
+        Log.d(LOG_TAG,"Sending log data");
         Proto logData = new Proto(Proto.LOG_DATA);
 
         logData.getData().put("PASS",password);
@@ -76,7 +78,7 @@ public class Connect extends AsyncTask {
         Proto response = readProto();
 
         if (response == null || response.getPerformative() == Proto.DENIED){
-            Log.d(LOGD_REF,(response == null)?"No response":"Server denied login");
+            Log.d(LOG_TAG,(response == null)?"No response":"Server denied login");
             return false;
         }
         else if (response.getPerformative() == Proto.ACCEPTED){
@@ -85,7 +87,7 @@ public class Connect extends AsyncTask {
             sendProto(ack);
         }
         else if (response.getPerformative() == Proto.NEW_ID){
-            Log.d(LOGD_REF,"New id received : "+(Integer)response.getData().get("ID"));
+            Log.d(LOG_TAG,"New id received : "+(Integer)response.getData().get("ID"));
             SharedPreferences.Editor editor = settings.edit();
             editor.putInt(Settings.ID, (Integer) response.getData().get("ID"));
             editor.commit();
@@ -97,11 +99,11 @@ public class Connect extends AsyncTask {
                 Proto ack = new Proto(Proto.ACK);
                 sendProto(ack);
             }else {
-                Log.d(LOGD_REF,(accepted == null)?"ACCEPTED not received":"Wrong performative, ACCEPTED expected [1]");
+                Log.d(LOG_TAG,(accepted == null)?"ACCEPTED not received":"Wrong performative, ACCEPTED expected [1]");
                 return false;
             }
         }else {
-            Log.d(LOGD_REF,"Wrong performative, ACCEPTED expected [2]");
+            Log.d(LOG_TAG,"Wrong performative, ACCEPTED expected [2]");
             return false;
         }
         return true;
@@ -178,25 +180,37 @@ public class Connect extends AsyncTask {
 
     @Override
     protected Object doInBackground(Object[] params) {
+
+        runnig.set(true);
+        if (runnig.get()){
+            return null;
+        }
+
         MainMenu main = (MainMenu)params[0];
         Context context = (Context) params[1];
         SharedPreferences settings = context.getSharedPreferences(Settings.FILENAME,Settings.MODE);
 
+        Log.d(LOG_TAG,"Connect:doInBackgroung");
+
         if (!checkInternetConnexion(main)){
             abortConnexion(main,"Connexion failed","No internet connexion");
+            runnig.set(false);
             return null;
         }
 
         if (!connexionToServer(settings)){
             abortConnexion(main,"Connexion failed","Failed to connect to the server");
+            runnig.set(false);
             return null;
         }
 
         if (!simpleLoggin(settings)){
             abortConnexion(main,"Connexion failed","Wrong password, change the settings and try again");
+            runnig.set(false);
             return null;
         }
         adminLogin(settings);
+        runnig.set(false);
         return null;
     }
 }
---
+++ Raspberry/TikiJava/src/LedManager.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Pablo on 19/07/2016.
+ */
+public class LedManager {
+}
