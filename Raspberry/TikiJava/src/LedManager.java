5c7529b71fa8a851c3fa42bc504bc05d015ff119
PabloPetit <pablopetit94@gmail.com>
2016-07-30 12:14:09 +0200

    commit

--- Raspberry/TikiJava/src/LedManager.java
+++ Raspberry/TikiJava/src/LedManager.java
@@ -1,5 +1,19 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Created by Pablo on 19/07/2016.
  */
 public class LedManager {
+
+
+
+
+
+
+    public AtomicBoolean locked;
+
+
+
+
+
 }
